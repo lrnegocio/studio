58abadecddfc70c2cbb229f71e43e32b12c7653c
Lenildo Romao <lrnegocio007@gmail.com>
2026-03-03 09:46:23 +0000

    Fix: Removendo Suspense infinito

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,9 +1,7 @@
-import { Suspense } from 'react';
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Suspense fallback={<div>Carregando...</div>}>{children}</Suspense>;
+  return <>{children}</>;
 }
